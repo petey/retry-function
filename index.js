d2a23a10f3884e9dc9c7bc59d1d91f723e530c8a
Peter Peterson <petey31@yahoo.com>
2016-10-17 14:45:42 -0700

    Update Joi

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 // Copyright 2015 Yahoo Inc.
 // Licensed under the MIT license.
 // See the included LICENSE file for terms.
 
-var retry = require('retry'),
-    joi = require('joi');
+const retry = require('retry');
+const joi = require('joi');
 
 /**
  * Executes retry operation
@@ -13,29 +15,38 @@ var retry = require('retry'),
  * @private
  */
 function execRetry(config, callback) {
-    config.shouldRetry = config.shouldRetry || function () {return true;};
-    config.options = config.options || {};
-    config.arguments = config.arguments || [];
-    config.context = config.context || null;
+    const c = config;
 
-    var operation = retry.operation(config.options);
+    c.shouldRetry = c.shouldRetry || function shouldRetry() {
+        return true;
+    };
+    c.options = c.options || {};
+    c.arguments = c.arguments || [];
+    c.context = c.context || null;
 
-    operation.attempt(function () {
-        var attemptArgs = config.arguments.slice();
+    const operation = retry.operation(c.options);
 
-        attemptArgs.push(function () {
-            var args = Array.prototype.slice.call(arguments),
-                err = args[0];
+    operation.attempt(() => {
+        const attemptArgs = c.arguments.slice();
 
-            if (config.shouldRetry(err) && operation.retry(err)) {
+        attemptArgs.push((...args) => {
+            const err = args[0];
+
+            if (c.shouldRetry(err) && operation.retry(err)) {
                 return;
             }
 
             callback.apply(null, args);
         });
 
-        config.method.apply(config.context, attemptArgs);
+        c.method.apply(c.context, attemptArgs);
     });
+    const a = [1, 2, 3, 4];
+    const b = { a, c };
+
+    a.push(5);
+
+    return b || a;
 }
 
 /**
@@ -46,29 +57,27 @@ function execRetry(config, callback) {
  * @private
  */
 function validateRetry(config, callback) {
-    var schema = joi.object().keys({
-            options: joi.object().keys({
-                retries: joi.number().optional(),
-                factor: joi.number().optional(),
-                minTimeout: joi.number().optional(),
-                maxTimeout: joi.number().optional(),
-                randomize: joi.boolean().optional(),
-            }).optional(),
-            context: joi.object().optional(),
-            arguments: joi.array().optional(),
-            shouldRetry: joi.func().optional(),
-            method: joi.func().required(),
-        }).required();
+    const schema = joi.object().keys({
+        options: joi.object().keys({
+            retries: joi.number().optional(),
+            factor: joi.number().optional(),
+            minTimeout: joi.number().optional(),
+            maxTimeout: joi.number().optional(),
+            randomize: joi.boolean().optional()
+        }).optional(),
+        context: joi.object().optional(),
+        arguments: joi.array().optional(),
+        shouldRetry: joi.func().optional(),
+        method: joi.func().required()
+    }).required();
 
     if (typeof callback !== 'function') {
         throw new Error('"callback" must be a function');
     }
 
-    joi.validate(config, schema, function (err) {
+    joi.validate(config, schema, (err) => {
         if (err) {
-            throw new Error(err.details.map(function (detail) {
-                return detail.message;
-            }).join(','));
+            throw new Error(err.details.map(detail => detail.message).join(','));
         }
         execRetry(config, callback);
     });
@@ -77,18 +86,18 @@ function validateRetry(config, callback) {
 /**
  * Retries a function call with exponential backoff
  * @method retryFn
- * @param  {Object}         config                      Retry-function configuration
- *     @param  {Function}   config.method               Asynchronous method to retry
- *     @param  {Function}   [config.shouldRetry=true]   Synchronous method takes an error object to determine if a retry is desired. (Boolean) fn(err). Defaults to always retry.
- *     @param  {Function}   config.callback             Function to call retry is complete
- *     @param  {Array}      config.arguments            Arguments that are supplied to the function being retried
- *     @param  {Object}     [config.context=null]       Context to supply to fn.apply() when calling method
- *     @param  {Object}     [config.options]            Retry configuration https://www.npmjs.com/package/retry
- *     @param  {Number}     [config.options.retries=10]             The maximum amount of times to retry the operation.
- *     @param  {Number}     [config.options.factor=2]               The exponential factor to use.
- *     @param  {Number}     [config.options.minTimeout=1000]        The number of milliseconds before starting the first retry.
- *     @param  {Number}     [config.options.maxTimeout=Infinity]    The maximum number of milliseconds between two retries.
- *     @param  {Boolean}    [config.options.randomize=false]        Randomizes the timeouts by multiplying with a factor between 1 to 2.
- * @param  {Function}       Callback                    Called when done fn(err, ...);
+ * @param  {Object}     config                      Retry-function configuration
+ * @param  {Function}   config.method               Asynchronous method to retry
+ * @param  {Function}   [config.shouldRetry=true]   Synchronous method takes an error object to determine if a retry is desired. (Boolean) fn(err). Defaults to always retry.
+ * @param  {Function}   config.callback             Function to call retry is complete
+ * @param  {Array}      config.arguments            Arguments that are supplied to the function being retried
+ * @param  {Object}     [config.context=null]       Context to supply to fn.apply() when calling method
+ * @param  {Object}     [config.options]            Retry configuration https://www.npmjs.com/package/retry
+ * @param  {Number}     [config.options.retries=10]             The maximum amount of times to retry the operation.
+ * @param  {Number}     [config.options.factor=2]               The exponential factor to use.
+ * @param  {Number}     [config.options.minTimeout=1000]        The number of milliseconds before starting the first retry.
+ * @param  {Number}     [config.options.maxTimeout=Infinity]    The maximum number of milliseconds between two retries.
+ * @param  {Boolean}    [config.options.randomize=false]        Randomizes the timeouts by multiplying with a factor between 1 to 2.
+ * @param  {Function}   Callback                    Called when done fn(err, ...);
  */
 module.exports = validateRetry;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,21 +1,28 @@
+'use strict';
+
 // Copyright 2015 Yahoo Inc.
 // Licensed under the MIT license.
 // See the included LICENSE file for terms.
 
-/*global describe, it */
-var sinon = require('sinon'),
-    Assert = require('chai').assert,
-    retryFn = require('../index');
+const sinon = require('sinon');
+const Assert = require('chai').assert;
+const retryFn = require('../index');
 
-describe('retry-function test case', function () {
-    describe('retryFn', function () {
-        it('should not retry if first run successful', function (done) {
-            var myFunc = sinon.stub(),
-                myCallback = function (err) {
-                    Assert.isNull(err);
-                    Assert.isTrue(myFunc.calledOnce);
-                    done();
-                };
+describe('retry-function test case', () => {
+    let myFunc;
+    let myCallback;
+
+    beforeEach(() => {
+        myFunc = sinon.stub();
+    });
+
+    describe('retryFn', () => {
+        it('should not retry if first run successful', (done) => {
+            myCallback = (err) => {
+                Assert.isNull(err);
+                Assert.isTrue(myFunc.calledOnce);
+                done();
+            };
 
             myFunc.yieldsAsync(null);
 
@@ -24,13 +31,12 @@ describe('retry-function test case', function () {
             }, myCallback);
         });
 
-        it('should retry fn until success', function (done) {
-            var myFunc = sinon.stub(),
-                myCallback = function (err) {
-                    Assert.isNull(err);
-                    Assert.isTrue(myFunc.calledThrice);
-                    done();
-                };
+        it('should retry fn until success', (done) => {
+            myCallback = (err) => {
+                Assert.isNull(err);
+                Assert.isTrue(myFunc.calledThrice);
+                done();
+            };
 
             myFunc.yieldsAsync(new Error('nope'));
             myFunc.onCall(2).yieldsAsync(null);
@@ -44,14 +50,13 @@ describe('retry-function test case', function () {
             }, myCallback);
         });
 
-        it('should have error if never succeeds', function (done) {
-            var myFunc = sinon.stub(),
-                myCallback = function (err) {
-                    Assert.isNotNull(err);
-                    Assert.equal(err.message, 'nope');
-                    Assert.isTrue(myFunc.calledThrice);
-                    done();
-                };
+        it('should have error if never succeeds', (done) => {
+            myCallback = (err) => {
+                Assert.isNotNull(err);
+                Assert.equal(err.message, 'nope');
+                Assert.isTrue(myFunc.calledThrice);
+                done();
+            };
 
             myFunc.yieldsAsync(new Error('nope'));
 
@@ -64,23 +69,20 @@ describe('retry-function test case', function () {
             }, myCallback);
         });
 
-        it('should not continue if shouldRetry says not to', function (done) {
-            var myFunc = sinon.stub(),
-                myCallback = function (err) {
-                    Assert.isNotNull(err);
-                    Assert.equal(err.message, 'yep');
-                    Assert.isTrue(myFunc.calledThrice);
-                    done();
-                };
+        it('should not continue if shouldRetry says not to', (done) => {
+            myCallback = (err) => {
+                Assert.isNotNull(err);
+                Assert.equal(err.message, 'yep');
+                Assert.isTrue(myFunc.calledThrice);
+                done();
+            };
 
             myFunc.yieldsAsync(new Error('nope'));
             myFunc.onCall(2).yieldsAsync(new Error('yep'));
 
             retryFn({
                 method: myFunc,
-                shouldRetry: function (err) {
-                    return err.message === 'nope';
-                },
+                shouldRetry: err => err.message === 'nope',
                 options: {
                     minTimeout: 10,
                     retries: 5
@@ -88,14 +90,14 @@ describe('retry-function test case', function () {
             }, myCallback);
         });
 
-        it('should fail if validation fails', function () {
-            Assert.throws(function () {
-                retryFn({}, function () {});
+        it('should fail if validation fails', () => {
+            Assert.throws(() => {
+                retryFn({}, () => ({}));
             }, Error, '"method" is required');
         });
 
-        it('should fail if callback missing', function () {
-            Assert.throws(function () {
+        it('should fail if callback missing', () => {
+            Assert.throws(() => {
                 retryFn({});
             }, Error, '"callback" must be a function');
         });
